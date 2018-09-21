1264eda297c0a0626c5ab05e17ecd0f30c4dd97f
Malrayess <m.alrayess@share.epsb.ca>
2018-09-21 13:08:23 -0600

    Update

---
+++ Swapping Variables/index.js
@@ -0,0 +1,8 @@
+let FirstName = "Maher";
+let lastName = "Al-Rayess";
+
+//new code goes here
+
+//Print variables to console
+console.log("My first name is: " + FirstName);
+console.log("My last name is: " + lastName);

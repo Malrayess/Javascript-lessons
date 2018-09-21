3284e29f95b9b37687eeb2dee87d4edc6ca24d35
Malrayess <m.alrayess@share.epsb.ca>
2018-09-21 13:15:30 -0600

    Update

--- Swapping Variables/index.js
+++ Swapping Variables/index.js
@@ -2,6 +2,10 @@ let FirstName = "Maher";
 let lastName = "Al-Rayess";
 
 //new code goes here
+// Add a third variable
+let memory = FirstName;
+FirstName = lastName;
+lastName = memory;
 
 //Print variables to console
 console.log("My first name is: " + FirstName);

719a3ae8d0bf62165c3d61f13b08cea497650655
Malrayess <m.alrayess@share.epsb.ca>
2018-09-25 13:53:41 -0600

    Update

--- Variables Arrays Start/index.js
+++ Variables Arrays Start/index.js
@@ -1,5 +1,14 @@
 console.log('Variables Object Start');
 
+//function goes here
+function switch (lastName1, lastName2) {
+  return MaherAlRayess[1] = lastName2;
+ }
+function switch2 (lastName1, lastName2) {
+   return MaherAlRayess2[1] = lastName2;
+ }
+
+
 let MaherAlRayess = [];
 let MaherAlRayess2 = [];
 
@@ -7,13 +16,10 @@ let MaherAlRayess2 = [];
 MaherAlRayess = ["Maher", "AlRayess", 15, true];
 MaherAlRayess2 = ["Maher2", "AlRayess2", 15, true];
 
-//printing out entire Object
-console.log(MaherAlRayess);
-console.log(MaherAlRayess2);
+// your code goes here
+switch (MaherAlRayess[1], MaherAlRayess2[1]);
+switch2 (MaherAlRayess2[1], MaherAlRayess[1]);
 
-//print last name only
-console.log("Last Name is: " + MaherAlRayess[1]);
-console.log("Last Name is: " + MaherAlRayess2[1]);
-
-MaherAlRayess [1]="Is Awesome";
-console.log(MaherAlRayess);
+// print last name only
+console.log("Last Name of MaherAlRayess is: " + MaherAlRayess[1]); // should return alrayess2
+console.log("Last Name of MaherAlRayess2 is: " + MaherAlRayess2[1]); // should return alrayess

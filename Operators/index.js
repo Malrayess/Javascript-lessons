bf8854a85449b892574aee831bb8ec0178e63c76
Malrayess <m.alrayess@share.epsb.ca>
2018-09-26 14:10:19 -0600

    Update

---
+++ Operators/index.js
@@ -0,0 +1,39 @@
+function simpleOperators (numberA, numberB) {
+  let adding = number[0] + number [1]
+  let subtract = number[0] - number [1]
+  let multiply = number[0] * number [1]
+  let divide = number[0] / number [1]
+  console.log(number);
+  console.log(adding);
+  console.log(subtract);
+  console.log(multiply);
+  console.log(divide);
+}
+
+let number = [4, 7, 28, 10];
+simpleOperators (number[0], number[1]);
+simpleOperators (number[2], number[3]);
+
+/*
+let skipCount = 0; // skip count by 2's to 10
+for (let i=1; i<=5; i++){
+  skipCount += 2;
+  console.log(skipCount);
+}
+*/
+
+/*
+let RskipCount = 15; // skip count by 3's backwards
+console.log(RskipCount);
+for (let i=1; i<=5; i++){
+  RskipCount -= 3;
+  console.log(RskipCount);
+}
+*/
+
+let MskipCount = 15; // skip count by
+console.log(MskipCount);
+for (let i=1; i<=5; i++){
+  MskipCount *= 3;
+  console.log(MskipCount);
+}

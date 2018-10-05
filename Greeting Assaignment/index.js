6ddcfb4c86efd877babdf34f0a143db9486689f7
Malrayess <m.alrayess@share.epsb.ca>
2018-10-05 13:31:59 -0600

    Update

--- Greeting Assaignment/index.js
+++ Greeting Assaignment/index.js
@@ -1,9 +1,14 @@
-let time = 13; //Change this simulating automatically reading time from Internet Clock or System Tray Clock
+let time = 9; //Change this simulating automatically reading time from Internet Clock or System Tray Clock
 console.log("The current time is ",time);
 
-/*
-if () console.log('Good Morning');
-else if () console.log('Good Afternoon');
-else if () console.console.log('Good Afternoon');
-else console.log();
-*/
+for (let i=0; i<24; i++) {
+  if (time < 12 && time >= 6) {
+    console.log('Good Morning');
+  } else if (time < 18 && time >= 12) {
+    console.log('Good Afternoon');
+  } else if (time < 23 && time >= 18) {
+    console.log('Good Evening');
+  } else if (time < 6 && time >= 0) {
+     console.log('Why am I awake at this time?');
+   }
+}

0f749750ee12f2b986e5d27bab4925467cf4015b
Malrayess <m.alrayess@share.epsb.ca>
2018-10-12 13:52:42 -0600

    Update

--- Ordering Grades Assignment/index.js
+++ Ordering Grades Assignment/index.js
@@ -1,20 +1,20 @@
 //Variable is an array for security
-const marks = []; //add grades as an array
+const marks = [47, 63, 76, 88, 96]; //add grades as an array
 
 //Use the calculatedGrade function as an output to the console
 //Note: example of using functions more than once (calculateAverage is used in console.log and calculatedGrade)
-console.log ("My letter grade is", calculatedGrade (average), "because my numerical average is", calculateAverage (marks));
+console.log ("My letter grade is", calculatedGrade (marks), "because my numerical average is", calculateAverage (marks));
 
 //This function uses another function, call Single Concern Function (able to use either function for various purposes)
 function calculatedGrade (marks) {
   const average = calculateAverage (marks);
-  if (average < 60) return ''; //Add appropriate grade or feedback
-  if () return ''; //Add approriate conditional
-  if () return '';
-  if () return '';
-  return A; //This is the last grade left, using the funnel image (metaphor)
+  if (average <= 100 && average >= 90) return 'A'; //Add appropriate grade or feedback
+  if (average <= 89 && average >= 80) return 'B'; //Add approriate conditional
+  if (average <= 79 && average >= 70) return 'C';
+  if (average <= 69 && average >= 60) return 'D';
+  return 'F'; //This is the last grade left, using the funnel image (metaphor)
 }
-function calculteAverage (array) {
+function calculateAverage (array) {
   let sum = 0;
   for (let mark of marks) sum += mark; //Example of reading an array and one line to add up the sum of all the grades
   return sum / marks.length; //"Divide the total marks by the number of grades"
